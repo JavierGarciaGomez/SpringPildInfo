7a756956f992d3bcb6663d1a0e2f0317681ef001
JGG <javieron.garcia@gmail.com>
2020-07-12 04:21:05 +0200

    first files

---
+++ Springl05/src/es/spring/ioc/JefeEmpleado.java
@@ -0,0 +1,9 @@
+package es.spring.ioc;
+
+public class JefeEmpleado {
+	
+	public String getTareas() {
+		return "Gestiono las cuestiones relativas a mis empleados";
+	}
+
+}
---
+++ Springl05/src/es/spring/ioc/Main.java
@@ -0,0 +1,11 @@
+package es.spring.ioc;
+
+public class Main {
+	public static void main (String[] args) {
+		JefeEmpleado empleado= new JefeEmmpleado();
+		
+	}
+	
+	
+
+}
