b1f53079feb18484e5f465e7a5ee7ecb6a315788
JGG <javieron.garcia@gmail.com>
2020-07-13 02:40:10 +0200

    Spring lessons 31-33 (MVC)

--- SpringMVC26/src/es/MVC/ControllerSayHi.java
+++ SpringMVC26/src/es/MVC/ControllerSayHi.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControllerSayHi {
@@ -20,11 +21,18 @@ public class ControllerSayHi {
 	}
 	
 	@RequestMapping("/processForm2") // 29
-	public String processForm2(HttpServletRequest request, Model model) {
-		String name=request.getParameter("name");
+	// public String processForm2(HttpServletRequest request, Model model) { 29
+	public String processForm2(@RequestParam("name") String name, Model model) { // 32
+		/*String name=request.getParameter("name"); 29
 		String finalMsg = "Who is the best student? "+name+" is the best";
 		// Adding info to the model
 		model.addAttribute("msg", finalMsg);
 		return "HiSpring";
+		*/
+		// 32
+		String finalMsg = "Who is the best student? "+name+" is the best";
+		model.addAttribute("msg", finalMsg);
+		return "HiSpring";
+		
 	}
 }
